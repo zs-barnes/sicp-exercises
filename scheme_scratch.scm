a0a9ac00f23da5291ece94df20d792f3d186d8ef
Zachary Barnes <zsbarnes@knox.edu>
2021-01-12 20:33:00 -0800

    Exercise 2.39

--- scheme_scratch.scm
+++ scheme_scratch.scm
@@ -1001,7 +1001,7 @@
       (op (car seq) (accumulate op initial (cdr seq)))))
 
 (define (reverse sequence)
-  (fold-right (lambda (x y)  (append  y  (list x)) nil sequence))
-(reverse (list 1 2 3))
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
 (define (reverse sequence)
   (fold-left (lambda (x y) (cons y x)) nil sequence))
